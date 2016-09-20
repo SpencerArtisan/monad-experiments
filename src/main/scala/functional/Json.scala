b2391f0b340e9d6bb0fd15f38c0363f2e98425f5
Spencer <spencerartisan@gmail.com>
2016-09-20 20:10:28 +0100

    Use Option rather than Xor

--- src/main/scala/functional/Json.scala
+++ src/main/scala/functional/Json.scala
@@ -1,11 +1,11 @@
 package functional
 
-import cats.data.Xor
-import cats.data.Xor._
+import scalaz._
+import Scalaz._
 
 class Json(private val data: Map[String, Any]) {
   def apply(key: String): Option[Any] =
-    if (data.contains(key)) Some(data(key)) else None
+    if (data.contains(key)) data(key).some else None
 
   override def toString: String =
     data.mkString("{\n", "\n", "\n}")
@@ -16,67 +16,66 @@ object Json {
   type JsonString = String
   type Converter[T] = String => T
   type Converters = Map[String, Converter[_]]
-  type Parser[A, B] = State[A] => Xor[Error, State[B]]
+  type Parser[A, B] = State[A] => Option[State[B]]
 
-  def parse(json: JsonString)(implicit converters: Converters = Map()): Xor[Error, Json] =
+  def parse(json: JsonString)(implicit converters: Converters = Map()): Option[Json] =
     if (json != null && !json.isEmpty)
       obj(State(json, Map())).map(state => new Json(state.data))
     else
-      Left("Empty json")
+      None
 
-  private def expr(state: State[Any])(implicit converters: Converters): Xor[Error, State[Any]] =
+  private def expr(state: State[Any])(implicit converters: Converters): Option[State[Any]] =
     text(state) orElse arr(state) orElse obj(state)
 
-  private def text(state: State[Any])(implicit converters: Converters): Xor[Error, State[String]] = {
+  private def text(state: State[Any])(implicit converters: Converters): Option[State[String]] = {
     val option = """^"(.+?)"""".r.findFirstMatchIn(state.jsonLeft)
     if (option.isDefined) {
       val valueString = option.get.group(1)
 //      val value = converter(valueString)
-      Right(State(state.jsonLeft.substring(option.get.group(0).length), valueString))
+      State(state.jsonLeft.substring(option.get.group(0).length), valueString).some
     } else
-      Left(s"Expected something of the form 'value', found '${truncate(state.jsonLeft)}'")
+      None
   }
 
-  private def arr(state: State[Any])(implicit converters: Converters): Xor[Error, State[List[Any]]] =
+  private def arr(state: State[Any])(implicit converters: Converters): Option[State[List[Any]]] =
     for {
       s1 <- symbol("[", state)
       s2 <- repeat(expr, s1, ",")
       s3 <- symbol("]", s2)
     } yield s3
 
-  private def obj(state: State[Any])(implicit converters: Converters): Xor[Error, State[Map[String, Any]]] =
+  private def obj(state: State[Any])(implicit converters: Converters): Option[State[Map[String, Any]]] =
     for {
       s1 <- symbol("{", state)
       s2 <- repeat(tuple, s1, ",")
       s3 <- symbol("}", s2)
     } yield State(s3.jsonLeft, s3.data.toMap)
 
-  private def repeat[B <: Any](parser: Parser[Any, B], state: State[Any], separator: String)(implicit converters: Converters): Xor[Error, State[List[B]]] = {
-    val first: Xor[Error, State[B]] = parser(state)
-    if (first.isLeft)
-      Right(State(state.jsonLeft, List()))
+  private def repeat[B <: Any](parser: Parser[Any, B], state: State[Any], separator: String)(implicit converters: Converters): Option[State[List[B]]] = {
+    val first = parser(state)
+    if (first.isEmpty)
+      State(state.jsonLeft, List()).some
     else {
-      val s4 = for {
-        s1 <- first
-        s2 <- symbol(separator, s1)
-        s3 <- repeat(parser, s2, separator)
-      } yield State(s3.jsonLeft, first.toOption.get.data +: s3.data)
-      if (s4.isRight) s4 else Right(State(first.toOption.get.jsonLeft, List(first.toOption.get.data)))
-    }
+      for {
+          s1 <- first
+          s2 <- symbol(separator, s1)
+          s3 <- repeat(parser, s2, separator)
+        } yield State(s3.jsonLeft, first.get.data +: s3.data)
+    } orElse first.map { s => State(s.jsonLeft, List(s.data)) }
   }
 
-  private def tuple(state: State[Any])(implicit converters: Converters): Xor[Error, State[(String, Any)]] =
+  private def tuple(state: State[Any])(implicit converters: Converters): Option[State[(String, Any)]] =
     for {
       s1 <- text(state)
       s2 <- symbol(":", s1)
       s3 <- expr(s2)
     } yield State(s3.jsonLeft, s1.data -> s3.data)
 
-  def symbol[A](symbol: String, state: State[A]): Xor[Error, State[A]] =
+  def symbol[A](symbol: String, state: State[A]): Option[State[A]] =
     if (state.jsonLeft.startsWith(symbol))
-      Right(state.advance(1))
+      state.advance(1).some
     else
-      Left(s"Expected '$symbol', found '${state.jsonLeft}'")
+      None
 
   private def truncate(text: String) =
     text.substring(0, Math.min(text.length(), 30))
--- src/test/scala/functional/JsonTest.scala
+++ src/test/scala/functional/JsonTest.scala
@@ -7,72 +7,72 @@ import functional.Json._
 
 class JsonTest extends FunSpec with Inside with Matchers with MockitoSugar {
   it("should not parse empty string") {
-    Json.parse("").isLeft should be (true)
+    Json.parse("").isEmpty should be (true)
   }
 
   it("should not parse null") {
-    Json.parse(null).isLeft should be (true)
+    Json.parse(null).isEmpty should be (true)
   }
 
   it("should not parse non-json") {
-    Json.parse("invalid").isLeft should be (true)
+    Json.parse("invalid").isEmpty should be (true)
   }
 
   it("should parse empty json") {
     val json = Json.parse("{}")
-    json.toOption.get("key") should be (None)
+    json.get("key") should be (None)
   }
 
   it("should parse json value") {
     val json = Json.parse("""{"key":"value"}""")
-    json.toOption.get("key") should be (Some("value"))
+    json.get("key") should be (Some("value"))
   }
 
   it("should parse empty json array") {
     val json = Json.parse("""{"key":[]}""")
-    json.toOption.get("key") should be (Some(List()))
+    json.get("key") should be (Some(List()))
   }
 
   it("should parse json array with empty object") {
     val json = Json.parse("""{"key":[{}]}""")
-    json.toOption.get("key").get should be (List(Map()))
+    json.get("key").get should be (List(Map()))
   }
 
   it("should parse json array with non-empty values") {
     val json = Json.parse("""{"key":["1"]}""")
-    json.toOption.get("key").get should be (List("1"))
+    json.get("key").get should be (List("1"))
   }
 
   it("should parse json array with non-empty objects") {
     val json = Json.parse("""{"key":[{"key2":"value"}]}""")
-    json.toOption.get("key").get should be (List(Map("key2" -> "value")))
+    json.get("key").get should be (List(Map("key2" -> "value")))
   }
 
   it("should parse json value with spaces") {
     val json = Json.parse("""{"key":"a value"}""")
-    json.toOption.get("key") should be (Some("a value"))
+    json.get("key") should be (Some("a value"))
   }
 
   it("should parse two json values") {
     val json = Json.parse("""{"key1":"value1","key2":"value2"}""")
-    json.toOption.get("key1") should be (Some("value1"))
-    json.toOption.get("key2") should be (Some("value2"))
+    json.get("key1") should be (Some("value1"))
+    json.get("key2") should be (Some("value2"))
   }
 
   it("should parse multiple json values") {
     val json = Json.parse("""{"key1":"value1","key2":"value2","key3":"value3"}""")
-    json.toOption.get.apply("key1") should be (Some("value1"))
-    json.toOption.get.apply("key2") should be (Some("value2"))
-    json.toOption.get.apply("key3") should be (Some("value3"))
+    json.get.apply("key1") should be (Some("value1"))
+    json.get.apply("key2") should be (Some("value2"))
+    json.get.apply("key3") should be (Some("value3"))
   }
 
   it("should use a custom value converter") {
     val json = Json.parse("""{"key":"42"}""")(Map("key" -> ((s:String) => s.toInt)))
-    json.toOption.get("key") should be (Some(42))
+    json.get("key") should be (Some(42))
   }
 
   it("should use a custom value converter 2") {
     val json = Json.parse("""{"key":"hello"}""")(Map("key" -> ((s:String) => s.toUpperCase)))
-    json.toOption.get("key") should be (Some("HELLO"))
+    json.get("key") should be (Some("HELLO"))
   }
 }
