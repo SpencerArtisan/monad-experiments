39495fe7b0bbbf1e94a24115b963e444cafa275f
Spencer <spencerartisan@gmail.com>
2016-09-20 19:38:50 +0100

    Genericise json

--- src/main/scala/functional/Json.scala
+++ src/main/scala/functional/Json.scala
@@ -16,7 +16,7 @@ object Json {
   type JsonString = String
   type Converter[T] = String => T
   type Converters = Map[String, Converter[_]]
-  type Parser = State => Xor[Error, State]
+  type Parser[A, B] = State[A] => Xor[Error, State[B]]
 
   def parse(json: JsonString)(implicit converters: Converters = Map()): Xor[Error, Json] =
     if (json != null && !json.isEmpty)
@@ -24,10 +24,10 @@ object Json {
     else
       Left("Empty json")
 
-  private def expr(state: State)(implicit converters: Converters): Xor[Error, State] =
+  private def expr(state: State[Any])(implicit converters: Converters): Xor[Error, State[Any]] =
     value(state) orElse arr(state) orElse obj(state)
 
-  private def value[T](state: State)(implicit converters: Converters): Xor[Error, State] = {
+  private def value(state: State[Any])(implicit converters: Converters): Xor[Error, State[String]] = {
     val option = """^"(.+?)"""".r.findFirstMatchIn(state.jsonLeft)
     if (option.isDefined) {
       val valueString = option.get.group(1)
@@ -37,28 +37,28 @@ object Json {
       Left(s"Expected something of the form 'value', found '${truncate(state.jsonLeft)}'")
   }
 
-  private def arr(state: State)(implicit converters: Converters): Xor[Error, State] =
+  private def arr(state: State[Any])(implicit converters: Converters): Xor[Error, State[List[Any]]] =
     for {
       s1 <- symbol("[", state)
       s2 <- repeat(expr, s1, ",")
       s3 <- symbol("]", s2)
     } yield s3
 
-  private def obj(state: State)(implicit converters: Converters): Xor[Error, State] =
+  private def obj(state: State[Any])(implicit converters: Converters): Xor[Error, State[Map[String, Any]]] =
     for {
       s1 <- symbol("{", state)
-      s2 <- repeat(mapEntry, s1, ",")
+      s2 <- repeat(tuple, s1, ",")
       s3 <- symbol("}", s2)
     } yield mapEntriesToMap(s3)
 
-  def mapEntriesToMap(state: State): State = {
+  def mapEntriesToMap(state: State[Any]): State[Map[String, Any]] = {
     val list1: List[Any] = state.data.asInstanceOf[List[Any]]
     val map: List[(String, Any)] = list1.map((entry) => entry.asInstanceOf[(String, Any)])
     State(state.jsonLeft, map.toMap)
   }
 
-  private def repeat(parser: Parser, state: State, separator: String)(implicit converters: Converters): Xor[Error, State] = {
-    val first: Xor[Error, State] = parser(state)
+  private def repeat(parser: Parser[Any, Any], state: State[Any], separator: String)(implicit converters: Converters): Xor[Error, State[List[Any]]] = {
+    val first: Xor[Error, State[Any]] = parser(state)
     if (first.isLeft)
       Right(State(state.jsonLeft, List()))
     else {
@@ -71,14 +71,14 @@ object Json {
     }
   }
 
-  private def mapEntry(state: State)(implicit converters: Converters): Xor[Error, State] =
+  private def tuple(state: State[Any])(implicit converters: Converters): Xor[Error, State[(String, Any)]] =
     for {
       s1 <- key(state)
       s2 <- symbol(":", s1)
       s3 <- expr(s2)
     } yield State(s3.jsonLeft, s1.data -> s3.data)
 
-  private def key(state: State): Xor[Error, State] = {
+  private def key(state: State[Any]): Xor[Error, State[String]] = {
     val option = """^"(.+?)"""".r.findFirstMatchIn(state.jsonLeft)
     if (option.isDefined) {
       val text = option.get.group(1)
@@ -87,7 +87,7 @@ object Json {
       Left(s"Expected something of the form 'key', found '${truncate(state.jsonLeft)}'")
   }
 
-  def symbol(symbol: String, state: State): Xor[Error, State] =
+  def symbol[A](symbol: String, state: State[A]): Xor[Error, State[A]] =
     if (state.jsonLeft.startsWith(symbol))
       Right(state.advance(1))
     else
@@ -96,12 +96,11 @@ object Json {
   private def truncate(text: String) =
     text.substring(0, Math.min(text.length(), 30))
 
-
-  case class State(jsonLeft: JsonString, data: Any) {
+  case class State[+T](jsonLeft: JsonString, data: T) {
     def advance(chars: Int) =
       State(jsonLeft.substring(chars), data)
 
-    def advance(chars: Int, newData: Any) =
+    def advance[U](chars: Int, newData: U) =
       State(jsonLeft.substring(chars), newData)
   }
 }
