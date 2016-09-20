0333f820abd69ab3b0797898a053cffb94a1bd30
Spencer <spencerartisan@gmail.com>
2016-09-20 19:48:24 +0100

    Minor tidying

--- src/main/scala/functional/Json.scala
+++ src/main/scala/functional/Json.scala
@@ -25,9 +25,9 @@ object Json {
       Left("Empty json")
 
   private def expr(state: State[Any])(implicit converters: Converters): Xor[Error, State[Any]] =
-    value(state) orElse arr(state) orElse obj(state)
+    text(state) orElse arr(state) orElse obj(state)
 
-  private def value(state: State[Any])(implicit converters: Converters): Xor[Error, State[String]] = {
+  private def text(state: State[Any])(implicit converters: Converters): Xor[Error, State[String]] = {
     val option = """^"(.+?)"""".r.findFirstMatchIn(state.jsonLeft)
     if (option.isDefined) {
       val valueString = option.get.group(1)
@@ -49,16 +49,10 @@ object Json {
       s1 <- symbol("{", state)
       s2 <- repeat(tuple, s1, ",")
       s3 <- symbol("}", s2)
-    } yield mapEntriesToMap(s3)
+    } yield State(s3.jsonLeft, s3.data.toMap)
 
-  def mapEntriesToMap(state: State[Any]): State[Map[String, Any]] = {
-    val list1: List[Any] = state.data.asInstanceOf[List[Any]]
-    val map: List[(String, Any)] = list1.map((entry) => entry.asInstanceOf[(String, Any)])
-    State(state.jsonLeft, map.toMap)
-  }
-
-  private def repeat(parser: Parser[Any, Any], state: State[Any], separator: String)(implicit converters: Converters): Xor[Error, State[List[Any]]] = {
-    val first: Xor[Error, State[Any]] = parser(state)
+  private def repeat[B <: Any](parser: Parser[Any, B], state: State[Any], separator: String)(implicit converters: Converters): Xor[Error, State[List[B]]] = {
+    val first: Xor[Error, State[B]] = parser(state)
     if (first.isLeft)
       Right(State(state.jsonLeft, List()))
     else {
@@ -73,19 +67,10 @@ object Json {
 
   private def tuple(state: State[Any])(implicit converters: Converters): Xor[Error, State[(String, Any)]] =
     for {
-      s1 <- key(state)
+      s1 <- text(state)
       s2 <- symbol(":", s1)
       s3 <- expr(s2)
     } yield State(s3.jsonLeft, s1.data -> s3.data)
-
-  private def key(state: State[Any]): Xor[Error, State[String]] = {
-    val option = """^"(.+?)"""".r.findFirstMatchIn(state.jsonLeft)
-    if (option.isDefined) {
-      val text = option.get.group(1)
-      Right(State(state.jsonLeft.substring(option.get.group(0).length), text))
-    } else
-      Left(s"Expected something of the form 'key', found '${truncate(state.jsonLeft)}'")
-  }
 
   def symbol[A](symbol: String, state: State[A]): Xor[Error, State[A]] =
     if (state.jsonLeft.startsWith(symbol))
